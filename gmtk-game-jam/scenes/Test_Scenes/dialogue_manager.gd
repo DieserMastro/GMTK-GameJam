86666fc1836948ef038c40a670dce87a60612a33
DieserMastro <117863339+DieserMastro@users.noreply.github.com>
2026-07-24 00:13:53 +0200

    Feat: base work for displaying Dialogue

---
+++ gmtk-game-jam/scenes/Test_Scenes/dialogue_manager.gd
@@ -0,0 +1,33 @@
+extends CanvasLayer
+
+@onready var dialogue_box: Control = $DialogueBox
+@onready var dialogue_text: Label = $DialogueBox/DialogueText
+
+var dialogue_lines: Array[String] = [];
+var current_line_index: int = 0;
+var is_dialogue_active: bool = false;
+
+func _ready() -> void:
+	dialogue_box.visible = false;
+	
+func start_dialogue(lines: Array[String]):
+	##SET GAME TO PAUSE!!!
+	dialogue_lines = lines;
+	current_line_index = 0;
+	is_dialogue_active = true;
+	dialogue_box.visible = true;
+	dialogue_text.text = dialogue_lines[current_line_index];
+	
+func _input(event: InputEvent) -> void:
+	if not is_dialogue_active:
+		return;
+	if event.is_action_pressed("interact"):
+		advance_dialogue()
+		
+func advance_dialogue():
+	if current_line_index < dialogue_lines.size()-1:
+		current_line_index += 1;
+		dialogue_text.text = dialogue_lines[current_line_index];
+	else
+		
+	
